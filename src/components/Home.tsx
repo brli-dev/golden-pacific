7dce0e3e7aabae113a55ac94baa5803b1182a21d
Brian Li <brli@bu.edu>
2024-03-05 19:34:34 -0500

    started on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Button from '../layouts/Button'
 
 const Home = () => {
   return (
-    <div>
+    <div className='min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 '>
         <div>
-            <h1>HOME</h1>
+            <h1>Elevate you tastes buds with every bite</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sit iusto voluptatem, quod voluptate voluptatum. Assumenda dicta beatae repudiandae. Sit quod aspernatur ab alias non est numquam libero unde cum?
+            </p>
+            <div>
+                <Button title="Order Now"/>
+            </div>
         </div>
     </div>
   )
