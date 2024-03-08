bc4de3166558d45363cd006d24d789b660a60617
Brian Li <brli@bu.edu>
2024-03-08 18:40:57 -0500

    replaced lorem ipsum with real text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,8 +8,9 @@ const About = () => {
         <img className="md:px-4 md:py-4 sm:px-4 sm:py-4" src={img} alt="img"/>
         <div className='space-y-4 lg:pt-14'>
             <h1 className='font-semibold text-4xl text-center md:text-start'>About Us</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non obcaecati ut officia dolor itaque, architecto iusto eaque ducimus nulla pariatur, repudiandae expedita quo et delectus nostrum ex reprehenderit. Ratione, totam.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ab id eos dolorem esse ipsam quaerat et voluptates, perferendis eveniet eum, similique at fugit porro quidem cupiditate pariatur corrupti quae.</p>
+            <p>At New Golden Pacific Holbrook we offer meals of excellent quality and invite you to try our delicious food.</p>
+            <p>The key to our success is simple: providing quality consistent food that taste great every single time. We pride ourselves on serving our customers delicious genuine dishes like: Chinese</p>
+            <p>Eat delicious food. Grab a drink. But most of all, relax! We thank you from the bottom of our hearts for your continued support.</p>
             <div className='flex justify-center lg:justify-start'>
                 <Button title="Learn More"/>
             </div>
